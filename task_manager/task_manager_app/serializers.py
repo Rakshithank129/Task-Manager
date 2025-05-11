e73cc8aae37f14f09eb34964da432ec16b60f3e1
RAKSHITHA N K <130299350+Rakshithank129@users.noreply.github.com>
2025-05-11 13:09:46 +0530

    Delete task_manager/task_manager_app directory

--- task_manager/task_manager_app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from task_manager_app.models import Task
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = '__all__'
